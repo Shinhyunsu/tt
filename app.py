375cf198faa66bf0efa0fbc9f45146e65e77c2d3
신현수 <sinhyeonsu@MacBook-Pro.local>
2021-11-21 15:00:38 +0900

    test-version0.009

--- app.py
+++ app.py
@@ -101,7 +101,8 @@ def whatever():
 
 
 if __name__ == "__main__":
-    scheduler.add_job(id="scheduler task", func = job, trigger='interval', minute =2)
+    #scheduler.add_job(id="scheduler task", func = job, trigger='interval', minute =2)
+    scheduler.add_job(job, 'cron', second='*/60', id="test_1")
     scheduler.start()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
