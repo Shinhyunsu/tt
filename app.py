42f23531d241224417b6f160d292652c392c2a64
신현수 <sinhyeonsu@MacBook-Pro.local>
2021-11-21 16:48:09 +0900

    test-version0.014

--- app.py
+++ app.py
@@ -6,79 +6,25 @@ import telegram
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import time
-from datetime import datetime
-#from flask_apscheduler import APScheduler
-#from apscheduler.schedulers.blocking import BlockingScheduler
 import schedule
-from threading import Thread
-
-start_time = time.time()
-
-def run_every_10_seconds():
-    print("Running periodic task!")
-    print ("Elapsed time: " + str(time.time() - start_time))
-
-def run_schedule():
-    
-    while 1:
-        
-        schedule.run_pending()
-        time.sleep(1)   
-
 
 app = Flask(__name__)
-#scheduler = APScheduler()
-#scheduler = BlockingScheduler()
 
 CORS(app)
 dataBuffer = []
 coinName =[]
 readData = ""
 
-def job():
-    print('time ok')
-
-
-
-
-@app.route('/',methods=['GET', 'OPTIONS'])
-def welcome():
-    
-    return render_template('index.html')
-
-@app.route('/webhook',methods=['POST'])
-def whatever():
-    global coinName
-    global dataBuffer
-    global readData
-    nowTime = datetime.now()
-    
-    
-    
-    
-    reqdata = json.loads(request.data)
-    print('pint',nowTime.hour,nowTime.minute, reqdata)
-    dataBuffer.append(reqdata)
-    
-
-    if nowTime.hour == 0 and  nowTime.minute >= 10 and nowTime.minute <= 20:
-        trig = True;
-        
-    else:
-        chkkk = False;
-        chkkkk = False;
-        trig = False;
-
-    if trig == True:
+def run_every():
+    print('asdg')
+    while len(dataBuffer) != 0:
         if len(dataBuffer) > 1:
             readData = dataBuffer.popleft();
             print('Processing... data -> ',readData)
         elif  len(dataBuffer) == 1:
             readData = dataBuffer.pop();
             print('Processing... data -> ',readData)
-        else :
-            trig = False;
-
+        
         for keyread in readData.keys():
             if keyread == 'moving_exchange':
                 chkkk = True
@@ -86,11 +32,7 @@ def whatever():
             if keyread == 'maxVolume':
                 chkkk = False
                 chkkkk = True
-            #if keyread == 'trigger_exchange':
-                #if trigger == True
 
-
-    
         if chkkk == True:
             chkkk = False;
             totalString = '🔔 코인 : ' + readData['moving_exchange'] + ' 거래량 : ' + str(readData['volume']) + ' 금액 : ' + str(readData['price']) + ' 시간 : ' + str(readData['time'])
@@ -109,6 +51,21 @@ def whatever():
             chat_id = '-1001678871735'
             bot.sendMessage(chat_id=chat_id, text=(totalString))
 
+@app.route('/',methods=['GET', 'OPTIONS'])
+def welcome():
+    
+    return render_template('index.html')
+
+@app.route('/webhook',methods=['POST'])
+def whatever():
+    global coinName
+    global dataBuffer
+    global readData
+    
+    
+    reqdata = json.loads(request.data)
+    dataBuffer.append(reqdata)
+    
     return {
         "code": "succss",
         "message": readData
@@ -116,19 +73,8 @@ def whatever():
 
 
 if __name__ == "__main__":
-    #scheduler.add_job(id="scheduler task", func = job, trigger='interval', minute =2)
-    # scheduler.add_job(job, 'cron', second='*/60', id="test_1")
-    #scheduler.add_job(func=job, trigger='cron', second='*/60')
-    #scheduler.start()
-
-    
-
-
-    print("main play")
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
 
-schedule.every(60).seconds.do(run_every_10_seconds)
-t = Thread(target=run_schedule)
-t.start()
+schedule.every().minute.at(":15").do(run_every)
